67809cfa64471dc46a89ae6c63d3f402add079bd
WeAthFolD <cheetah77@qq.com>
2015-10-22 17:01:25 +0800

    Cleanup for build

--- src/main/java/cn/liutils/core/LIUtils.java
+++ src/main/java/cn/liutils/core/LIUtils.java
@@ -46,7 +46,7 @@ public class LIUtils {
 		REGISTER_TYPE_RENDER_HOOK = "liu_playerhook",
 		REGISTER_TYPE_CONFIGURABLE = "liu_configurable";
 	
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "1.0";
 	
 	/**
 	 * Does open debug mode. turn to false when compiling.
