331a3518d4dc88dfca671a81bb8e3a2135b6c39d
WeAthFolD <cheetah77@qq.com>
2015-11-18 20:27:39 +0800

    Change build version, minor bugfixes

--- src/main/java/cn/lambdalib/core/LambdaLib.java
+++ src/main/java/cn/lambdalib/core/LambdaLib.java
@@ -37,7 +37,7 @@ import net.minecraft.command.CommandHandler;
 @RegistrationMod(pkg = "cn.lambdalib.", res = "lambdalib", prefix = "ll_")
 public class LambdaLib {
 
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1_dev";
 
 	/**
 	 * Does open debug mode. turn to false when compiling.
