ab1d855da1053c2c3647a2b7e854fdc9281d6a4d
WeAthFolD <cheetah77@qq.com>
2016-01-29 20:56:52 +0800

    Make shader fail gracefully
    * useProgram does nothing when compile fails
    * use isValid() to query compile status

--- src/main/java/cn/lambdalib/util/client/shader/ShaderProgram.java
+++ src/main/java/cn/lambdalib/util/client/shader/ShaderProgram.java
@@ -21,6 +21,7 @@ import cn.lambdalib.util.generic.RegistryUtils;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 /**
  * A simple GL Shader Program wrapper.
@@ -29,7 +30,7 @@ import net.minecraft.util.ResourceLocation;
 @SideOnly(Side.CLIENT)
 public class ShaderProgram {
     
-    static Map<ResourceLocation, Integer> loadedShaders = new HashMap();
+    static Map<ResourceLocation, Integer> loadedShaders = new HashMap<>();
     public static void releaseResources() {
         for(Integer e : loadedShaders.values())
             glDeleteShader(e);
@@ -37,18 +38,25 @@ public class ShaderProgram {
     }
     
     private boolean compiled = false;
+    private boolean valid = false;
     private int programID;
-    private List<Integer> attachedShaders = new ArrayList();
+    private List<Integer> attachedShaders = new ArrayList<>();
     
     public ShaderProgram() {
         programID = glCreateProgram();
     }
     
     public void linkShader(ResourceLocation location, int type) {
+        if (!checkCapability())
+            return;
+
         try {
             int shaderID;
+
+            boolean loaded;
             if(loadedShaders.containsKey(location)) {
                 shaderID = loadedShaders.get(location);
+                loaded = true;
             } else {
                 String str = IOUtils.toString(RegistryUtils.getResourceStream(location));
                 shaderID = glCreateShader(type);
@@ -59,14 +67,18 @@ public class ShaderProgram {
                 if(successful == GL_FALSE) {
                     String log = glGetShaderInfoLog(shaderID, glGetShaderi(shaderID, GL_INFO_LOG_LENGTH));
                     LambdaLib.log.error("Error when linking shader '" + location + "'. code: " + successful + ", Error string: \n" + log);
-                    throw new RuntimeException();
+                    loaded = false;
+                } else {
+                    loaded = true;
                 }
             }
-            
-            attachedShaders.add(shaderID);
-            glAttachShader(programID, shaderID);
+
+            if (loaded) {
+                attachedShaders.add(shaderID);
+                glAttachShader(programID, shaderID);
+            }
         } catch (IOException e) {
-            LambdaLib.log.error("Error when linking shader " + location, e);
+            LambdaLib.log.error("Didn't find shader " + location, e);
             throw new RuntimeException();
         }
     }
@@ -76,12 +88,12 @@ public class ShaderProgram {
     }
     
     public void useProgram() {
-        if(compiled) {
+        if(compiled && valid) {
             glUseProgram(programID);
-        } else {
+        } else if (!compiled) {
             LambdaLib.log.error("Trying to use a uncompiled program");
             throw new RuntimeException();
-        }
+        } // not valid, ignore the shader usage
     }
     
     public int getUniformLocation(String name) {
@@ -89,6 +101,11 @@ public class ShaderProgram {
     }
     
     public void compile() {
+        if (!checkCapability()) {
+            compiled = true;
+            return;
+        }
+
         if(compiled) {
             LambdaLib.log.error("Trying to compile shader " + this + " twice.");
             throw new RuntimeException();
@@ -105,16 +122,25 @@ public class ShaderProgram {
             String log = glGetProgramInfoLog(programID, glGetProgrami(programID, GL_INFO_LOG_LENGTH));
             LambdaLib.log.error("Error when linking program #" + programID + ". Error code: " + status + ", Error string: ");
             LambdaLib.log.error(log);
-            throw new RuntimeException();
+            valid = false;
+        } else {
+            valid = true;
         }
         
         compiled = true;
     }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    private boolean checkCapability() {
+        String versionShort = GL11.glGetString(GL11.GL_VERSION).trim().substring(0, 3);
+        return "2.1".compareTo(versionShort) <= 0;
+    }
     
     /**
      * Get the src of a shader in lambdalib namespace.
-     * @param name
-     * @return
      */
     public static ResourceLocation getShader(String name) {
         return new ResourceLocation("lambdalib:shaders/" + name);
