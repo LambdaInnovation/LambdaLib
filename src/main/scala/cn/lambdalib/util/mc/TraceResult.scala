c9478d8d90e2d609b16921c2739e1c12224ad9b9
WeAthFolD <cheetah77@qq.com>
2016-02-28 18:11:28 +0800

    Add position info for TraceResult

--- src/main/scala/cn/lambdalib/util/mc/TraceResult.scala
+++ src/main/scala/cn/lambdalib/util/mc/TraceResult.scala
@@ -1,7 +1,7 @@
 package cn.lambdalib.util.mc
 
 import net.minecraft.entity.Entity
-import net.minecraft.util.MovingObjectPosition
+import net.minecraft.util.{Vec3, MovingObjectPosition}
 import net.minecraft.util.MovingObjectPosition.MovingObjectType
 import net.minecraft.world.World
 
@@ -17,14 +17,30 @@ object TraceResult {
   }
 }
 
-trait TraceResult
+trait TraceResult {
+  def hasPosition: Boolean
+  def position: Vec3
+}
 
-case class EmptyResult() extends TraceResult
+case class EmptyResult() extends TraceResult {
+  override def hasPosition = false
+  override def position = throw new UnsupportedOperationException()
+}
 
-case class EntityResult(target: Entity) extends TraceResult
+case class EntityResult(target: Entity) extends TraceResult {
+  import MCExtender._
+
+  override def hasPosition = true
+  override def position = target.position
+}
 
 case class BlockResult(pos: (Int, Int, Int), side: Int) extends TraceResult {
+  import MCExtender._
+
   def getBlock(implicit world: World) = world.getBlock(pos._1, pos._2, pos._3)
   def getTileEntity(implicit world: World) = world.getTileEntity(pos._1, pos._2, pos._3)
+
+  override def hasPosition = true
+  override def position    = (pos._1 + .5, pos._2 + .5, pos._3 + .5)
 }
 
