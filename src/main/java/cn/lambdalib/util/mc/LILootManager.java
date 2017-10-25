7ca2e8385e2eda7ad70e915a115394a498fd04e6
Paindar <1944798085@qq.com>
2017-10-25 13:34:49 +0800

    add missing event about loading lootPool

--- src/main/java/cn/lambdalib/util/mc/LILootManager.java
+++ src/main/java/cn/lambdalib/util/mc/LILootManager.java
@@ -1,10 +1,15 @@
 package cn.lambdalib.util.mc;
 
+import cn.lambdalib.annoreg.core.LoadStage;
 import cn.lambdalib.annoreg.core.Registrant;
-import cn.lambdalib.annoreg.mc.RegEventHandler;
+import cn.lambdalib.annoreg.mc.RegCallback;
 import net.minecraft.world.storage.loot.LootEntry;
+import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.RandomValueRange;
+import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.ArrayList;
@@ -18,18 +23,36 @@ import java.util.Map;
 @Registrant
 public class LILootManager {
 
-    @RegEventHandler(RegEventHandler.Bus.Forge)
     private static LILootManager instance=new LILootManager();
     private Map<String,List<LootEntry>> typeMap = new HashMap<>();
 
+    @RegCallback(stage= LoadStage.PRE_INIT)
+    public static void preInit(FMLPreInitializationEvent evt){
+        MinecraftForge.EVENT_BUS.register(instance);
+    }
+
     @SubscribeEvent
     public void onLootLoad(LootTableLoadEvent evt) {
-
+        register(evt);
     }
 
     public static LILootManager getInstance()
     {
         return instance;
+    }
+
+    public void register(LootTableLoadEvent evt){
+        for(Map.Entry<String,List<LootEntry>>entry:typeMap.entrySet()){
+            if (evt.getName().toString().contains(entry.getKey())) {
+                LootEntry[] ets=new LootEntry[entry.getValue().size()];
+                entry.getValue().toArray(ets);
+                LootPool pool = new LootPool(ets, new LootCondition[]{(loot, ctx)->true},
+                        new RandomValueRange(1), new RandomValueRange(0),"LLib_LootPool"); // Other params set as you wish.
+                evt.getTable().addPool(pool);
+                break;
+            }
+        }
+
     }
 
     public void add(String type, LootEntry entry){
